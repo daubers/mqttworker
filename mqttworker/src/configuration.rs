52daad7201c735ec789b4a854120e6998b3fb234
Matthew Daubney <matt@daubney.dev>
2026-07-25 23:14:40 +0100

    feat(Worker): Add configuration file support and upgrade dependencies
    - Introduce a new configuration module to load settings from a TOML file.
    - Update `paho-mqtt` dependency to v0.14.0 across all crates.
    - Refactor MQTT connection logic to use configuration values like broker URI and credentials.
    - Add support for passing a configuration file via CLI arguments.
    - Modify async worker process to use configuration for MQTT client initialization.
    - Update lockfile to include new dependencies (`clap`, `toml`, etc.).

---
+++ mqttworker/src/configuration.rs
@@ -0,0 +1,40 @@
+use std::fs;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Config {
+    pub node_name: String,
+    pub broker: Broker,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Credentials {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Broker {
+    pub broker_uri: String, // "mqtt://localhost:1883"
+    #[serde(default="default_broker_auth")]
+    pub broker_authenticate: bool,
+    #[serde(default="default_credentials")]
+    pub credentials: Option<Credentials>
+}
+
+fn default_broker_auth() -> bool {
+    true
+}
+
+fn default_credentials() -> Option<Credentials> {
+    None
+}
+
+impl Config {
+    pub fn new(config_path: &str) -> Config {
+        let file_contents = fs::read_to_string(config_path).expect("Can't read config file");
+        let config: Config = toml::from_str(file_contents.as_str()).expect("Can't parse config file");
+        config
+    }
+}
--- mqttworker/src/main.rs
+++ mqttworker/src/main.rs
@@ -1,23 +1,45 @@
 pub mod mqtt;
+pub mod configuration;
+
+use std::sync::Arc;
+use clap::Parser;
 
 use tokio;
 use tokio::signal::unix::{signal, SignalKind};
 use tokio_cron_scheduler::{Job, JobScheduler};
 use messages::messages::CapabilitiesMessage;
-use crate::mqtt::{connect_client_async, connect_client_sync};
+use crate::configuration::Config;
+use crate::mqtt::connect_client_async;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    configuration_file: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let mqtt_client = connect_client_async(true).await.unwrap();
+    let args = Args::parse();
+    let configuration = Arc::new(Config::new(&args.configuration_file));
+    let mqtt_client = connect_client_async(configuration, true).await.unwrap();
     let mut scheduler = JobScheduler::new().await.expect("Can't initialise scheduler");
+    let conf_file = args.configuration_file.clone();
     scheduler.add(
-        Job::new_async("1/5 * * * * *", |uuid, mut _l| {
-            let mqtt_client_c = connect_client_sync(false).expect("Can't connect sync mqtt client");
-            Box::pin(async move {
-                let capabilities_announcement = CapabilitiesMessage::new("test".to_string());
-                mqtt_client_c.publish(capabilities_announcement.message()).expect("Can't publish message");
-                mqtt_client_c.disconnect(None).expect("Can't disconnect");
-            })
+        Job::new_async("1/5 * * * * *", move |_uuid, mut _l| {
+            Box::pin({
+            {
+            let value = conf_file.clone();
+            async move {
+                let conf = Arc::new(Config::new(value.as_str()));
+                let mqtt_client_c = connect_client_async(conf.clone(), false).await.expect("Can't connect sync mqtt client");
+
+                let node_name = conf.node_name.clone().as_str().to_string();
+                let capabilities_announcement = CapabilitiesMessage::new(node_name);
+                mqtt_client_c.publish(capabilities_announcement.message()).await.expect("Can't publish message");
+                mqtt_client_c.disconnect(None).await.expect("Can't disconnect");
+            }}})
         }
         ).expect("Can't add job")
     ).await.expect("Can't add job");
