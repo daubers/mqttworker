33b9c8bb74fe37b74ac265155a7c4fd1820e5887
Matthew Daubney <matt@daubney.dev>
2026-07-25 23:14:40 +0100

    feat(Worker): Add configuration file support and upgrade dependencies
    - Introduce a new configuration module to load settings from a TOML file.
    - Update `paho-mqtt` dependency to v0.14.0 across all crates.
    - Refactor MQTT connection logic to use configuration values like broker URI and credentials.
    - Add support for passing a configuration file via CLI arguments.
    - Modify async worker process to use configuration for MQTT client initialization.
    - Update lockfile to include new dependencies (`clap`, `toml`, etc.).

--- mqttworker/src/mqtt.rs
+++ mqttworker/src/mqtt.rs
@@ -1,19 +1,30 @@
 use std::env;
+use std::sync::Arc;
 use paho_mqtt as mqtt;
 use messages::messages::{WorkerAnnouncement, WorkerAnnouncementType};
+use crate::configuration::Config;
 
-pub async fn connect_client_async(send_online: bool) -> Result<mqtt::AsyncClient, paho_mqtt::Error> {
+pub async fn connect_client_async(configuration: Arc<Config>, send_online: bool) -> Result<mqtt::AsyncClient, paho_mqtt::Error> {
     // Command-line option(s)
-    let host = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "mqtt://localhost:1883".to_string());
+    let mqtt_connect_options = mqtt::ConnectOptionsBuilder::new()
+        .user_name(configuration.broker.credentials.clone().unwrap().username.clone())
+        .password(configuration.broker.credentials.clone().unwrap().password.clone())
+        .finalize();
+
+    let mqtt_create_opts = mqtt::CreateOptionsBuilder::new()
+        .server_uri(configuration.broker.broker_uri.clone())
+        .finalize();
 
     // Create the client
-    let cli = mqtt::AsyncClient::new(host).expect("Can't create client");
+    let cli = mqtt::AsyncClient::new(mqtt_create_opts).expect("Can't create client");
 
     // Connect with default options and wait for it to complete or fail
     // The default is an MQTT v3.x connection.
-    cli.connect(None).await.expect("Can't connect");
+    if configuration.broker.broker_authenticate {
+        cli.connect(mqtt_connect_options).await.expect("Can't connect");
+    } else {
+        cli.connect(None).await.expect("Can't connect");
+    }
     if send_online {
         let msg = WorkerAnnouncement::new(None, "worker_tmp_id".to_string(), WorkerAnnouncementType::Online, None);
         cli.publish(msg.message()).await.expect("Can't publish");
@@ -32,8 +43,14 @@ pub fn connect_client_sync(send_online: bool) -> Result<mqtt::Client, paho_mqtt:
 
     // Connect with default options and wait for it to complete or fail
     // The default is an MQTT v3.x connection.
-    cli.connect(None).expect("Can't connect");
-    if send_online {
+    let cli = match cli.connect(None) {
+        Ok(_) => cli,
+        Err(err) => {
+            println!("Error connecting to MQTT broker: {:#?}", err);
+            cli
+        },
+    };
+    if send_online && cli.is_connected() {
         let msg = WorkerAnnouncement::new(None, "worker_tmp_id".to_string(), WorkerAnnouncementType::Online, None);
         cli.publish(msg.message()).expect("Can't publish");
     }
