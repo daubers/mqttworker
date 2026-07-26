268ca48d04e9681710c4a1383dc746471e3b691a
Matthew Daubney <matt@daubney.dev>
2026-07-26 22:40:26 +0100

    feat(Worker): Add topic subscriptions, message handling, and improved signal handling
    - Implement MQTT topic subscriptions with QoS and retained flags.
    - Enhance message handling with a reconnect mechanism in case of disconnection.
    - Refactor signal handling into a separate async task.
    - Update configuration to include configurable subscription topics.
    - Introduce additional dependencies (`smol`, etc.) for improved async capabilities.

--- mqttworker/src/main.rs
+++ mqttworker/src/main.rs
@@ -2,15 +2,17 @@ pub mod mqtt;
 pub mod configuration;
 
 use std::sync::Arc;
+use std::time::Duration;
 use clap::Parser;
 
 use tokio;
 use tokio::signal::unix::{signal, SignalKind};
+use tokio::task;
 use tokio_cron_scheduler::{Job, JobScheduler};
 use messages::messages::CapabilitiesMessage;
 use crate::configuration::Config;
 use crate::mqtt::connect_client_async;
-
+const QOS: &[i32] = &[1, 1];
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -23,7 +25,7 @@ struct Args {
 async fn main() {
     let args = Args::parse();
     let configuration = Arc::new(Config::new(&args.configuration_file));
-    let mqtt_client = connect_client_async(configuration, true).await.unwrap();
+    let mqtt_client = connect_client_async(configuration.clone(), true).await.unwrap();
     let mut scheduler = JobScheduler::new().await.expect("Can't initialise scheduler");
     let conf_file = args.configuration_file.clone();
     scheduler.add(
@@ -37,8 +39,8 @@ async fn main() {
 
                 let node_name = conf.node_name.clone().as_str().to_string();
                 let capabilities_announcement = CapabilitiesMessage::new(node_name);
-                mqtt_client_c.publish(capabilities_announcement.message()).await.expect("Can't publish message");
-                mqtt_client_c.disconnect(None).await.expect("Can't disconnect");
+                mqtt_client_c.client.publish(capabilities_announcement.message()).await.expect("Can't publish message");
+                mqtt_client_c.client.disconnect(None).await.expect("Can't disconnect");
             }}})
         }
         ).expect("Can't add job")
@@ -47,16 +49,43 @@ async fn main() {
     // Start the scheduler
     scheduler.start().await.expect("Can't start scheduler");
 
-    let mut sig_int_handler = signal(SignalKind::interrupt()).expect("Can't create signal");
-    let mut sig_term_handler = signal(SignalKind::terminate()).expect("Can't create signal");
-    let mut sig_hup_handler = signal(SignalKind::hangup()).expect("Can't create signal");
-    // Wait while the jobs run
-    tokio::select! {
-        _ = sig_int_handler.recv() => println!("SIGINT"),
-        _ = sig_term_handler.recv() => println!("SIGTERM"),
-        _ = sig_hup_handler.recv() => println!("SIGHUP"),
+    println!("Subscribing to topics: {:?}", configuration.broker.topics);
+    let sub_opts = vec![paho_mqtt::SubscribeOptions::with_retain_as_published(); configuration.broker.topics.len()];
+    mqtt_client.client.subscribe_many_with_options(&*configuration.broker.topics, QOS, &sub_opts, None)
+        .await.expect("TODO: panic message");
+
+    let _shutdown_task = task::spawn_blocking(async move || {
+        let mut sig_int_handler = signal(SignalKind::interrupt()).expect("Can't create signal");
+        let mut sig_term_handler = signal(SignalKind::terminate()).expect("Can't create signal");
+        let mut sig_hup_handler = signal(SignalKind::hangup()).expect("Can't create signal");
+
+        // Wait while the jobs run
+        tokio::select! {
+            _ = sig_int_handler.recv() => println!("SIGINT"),
+            _ = sig_term_handler.recv() => println!("SIGTERM"),
+            _ = sig_hup_handler.recv() => println!("SIGHUP"),
+        }
+        println!("terminating the process...");
+        let _ = scheduler.shutdown();
+        //mqtt_client.client.disconnect(None).await.expect("Can't disconnect client");
+    }).await.expect("TODO: panic message");
+
+    while let Ok(msg_opt) = mqtt_client.message_stream.recv().await {
+        if let Some(msg) = msg_opt {
+            if msg.retained() {
+                print!("(R) ");
+            }
+            println!("{}", msg);
+        }
+        else {
+            // A "None" means we were disconnected. Try to reconnect...
+            println!("Lost connection. Attempting reconnect.");
+            while let Err(err) = mqtt_client.client.reconnect().await {
+                println!("Error reconnecting: {}", err);
+                smol::Timer::after(Duration::new(10, 0)).await;
+            }
+        }
     }
-    println!("terminating the process...");
-    let _ = scheduler.shutdown();
-    mqtt_client.disconnect(None).await.expect("Can't disconnect client");
+
+
 }
