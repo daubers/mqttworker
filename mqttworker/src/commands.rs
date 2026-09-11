986275528655d5f85a56bab59aa5547b08339957
Matthew Daubney <matt@daubney.dev>
2026-09-11 10:48:05 +0100

    refactor(worker): Extract job handling into a dedicated `commands` module
    - Move `start_job` logic to a new `commands` module for improved modularity.
    - Simplify `main.rs` by delegating command handling to `commands::start_job`.

---
+++ mqttworker/src/commands.rs
@@ -0,0 +1,24 @@
+use crate::configuration::{JobDefinition, Task};
+use crate::containers::run_task;
+use messages::messages::WorkerStartJobMessage;
+use paho_mqtt::Message;
+use regex::Captures;
+
+pub async fn start_job(msg: &Message, cmd: Captures<'_>) {
+    let command = cmd.name("cmd").unwrap().as_str();
+    println!("Worker command: {:#?}", command);
+    match command {
+        "startjob" => {
+            println!("Starting job");
+            let job_full: WorkerStartJobMessage = serde_json::from_str(msg.payload_str().to_string().as_str()).unwrap();
+            let job_definition = JobDefinition::from_str(job_full.workflow.as_str());
+            let tasks: Task = match job_definition.job.first_key_value() {
+                None => { panic!() }
+                Some((_k, v)) => v.clone()[0].clone()
+            };
+            run_task(tasks).await;
+        }
+        "stopjob" => { println!("Stopping job") }
+        _ => { println!("Unknown worker command") }
+    }
+}
